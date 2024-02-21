dec1b61e7999bbf40f3ef8bd25d51e1b64e4fb56
Leyman Max <jiumoh2011@yandex.ru>
2024-02-21 21:42:43 +0300

    change example + clean code

--- build.rs
+++ build.rs
@@ -1,15 +1,5 @@
 
 
 fn main() {
-    // let mut conf = cpp_build::Config::new();
-    // conf.include("include/qt")
-    //     .opt_level(2)
-    //     .build("src/main.rs");
-
-    // println!("cargo:rustc-link-lib=Qt5Gui");
-    // println!("cargo:rustc-link-lib=Qt5Core");
-    // println!("cargo:rustc-link-lib=Qt5Quick");
-    // println!("cargo:rustc-link-lib=Qt5Qml");
-    
     slint_build::compile("ui/appwindow.slint").unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 slint::include_modules!();
 
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
+    ui.on_exit(
         move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+            println!("User exit.");
+            std::process::exit(0);
         }
-    });
+    );
 
     ui.run()
 }

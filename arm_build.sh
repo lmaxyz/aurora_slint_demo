077838da0eddccf629abeeb0a68f46cebcd69a27
Maxim Leyman <maximleyman@yandex.ru>
2025-05-23 16:57:19 +0300

    update build script + update dependencies + make demo app available for device

---
+++ arm_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$PSDK_DIR" ]; then
+    echo '`PSDK_DIR` not found.';
+    exit 1;
+fi
+
+
+CURRENT_DIR="$(pwd)";
+TARGET=armv7-unknown-linux-gnueabihf
+
+aurora_psdk="$PSDK_DIR/sdk-chroot"
+
+mkdir -p RPMS/
+
+cross build --release --target $TARGET
+cargo generate-rpm -a armv7hl --target $TARGET -o RPMS/
+
+$aurora_psdk rpmsign-external sign -k $PSDK_DIR/../../certs/regular_key.pem -c $PSDK_DIR/../../certs/regular_cert.pem $CURRENT_DIR/RPMS/com.lmaxyz.LetsHabr-0.2.0-1.armv7hl.rpm
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ slint::include_modules!();
 
 
 fn main() -> Result<(), slint::PlatformError> {
+    std::env::set_var("SLINT_DEBUG_PERFORMANCE", "refresh_full_speed,overlay");
+
     let ui = AppWindow::new()?;
     ui.run()
 }
